88f3003121723929cf78dc68a1edc41ce5153bb5
SurroKun <surrokun@gmail.com>
2019-12-16 00:12:16 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/exam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exam.dir/main.cpp.o"
+  "exam.exe"
+  "exam.pdb"
+  "libexam.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
